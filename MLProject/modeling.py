e4475d8e99971499c720f36b559db6f8773c1759
aldozeepriyadi <aldo.fernandosupriyadi@gmail.com>
2025-06-04 22:09:34 +0700

    Tambah Docker 8

--- MLProject/modeling.py
+++ MLProject/modeling.py
@@ -33,7 +33,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # === Mulai MLflow run ===
 with mlflow.start_run(run_name="RandomForest_Default"):
-    mlflow.autolog()
+   
 
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
